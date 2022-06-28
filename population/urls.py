4772b5ca0f7a9585f57456a0834464018663b926
Erick Andrade <aguilakrakatoa@gmail.com>
2022-06-28 11:56:38 -0400

    add SearchBar

--- population/urls.py
+++ population/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import cities, delete_province
+from .views import cities, delete_province, searchbar
 
 
 urlpatterns = [
     path('', cities),
-    path('delete_province/<int:data_id>/', delete_province, name='delete_province')
+    path('delete_province/<int:data_id>/', delete_province, name='delete_province'),
+    path('searchbar/', searchbar, name='searchbar')
 ]
--- population/views.py
+++ population/views.py
@@ -1,3 +1,6 @@
+from contextlib import nullcontext
+from queue import Empty
+from unicodedata import name
 from django.shortcuts import render, redirect
 from .models import Population
 from django.http import HttpResponse
@@ -6,10 +9,23 @@ import datetime
 def cities(request):
     cities = Population.objects.all()
     return render(request, 'landing.html', {"cities": cities})
-    # return HttpResponse(msg, content_type='text/plain')
 
 def delete_province(request, data_id):
     province = Population.objects.get(id=data_id)
     province.delete()
     print(data_id)
     return redirect('/')
+
+def searchbar(request):
+    nameOfCity= ''
+    
+    if request.method == 'POST':
+        search = request.POST.get('searchbar')
+        nameOfCity = Population.objects.all().filter(city=search)
+    # now = datetime.datetime.now() 
+    # msg = f'Today is {now} and {nameOfCity.values_list()} and your type {search}'
+    # return redirect('/', {"searchData": nameOfCity})
+    # return HttpResponse(msg, content_type='text/plain')
+    return render(request, 'landing.html', {"searchData": nameOfCity})
+    
+    
