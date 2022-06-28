3e09a0b6f222b5f93feab525ba2eb3c453f0786f
Erick Andrade <aguilakrakatoa@gmail.com>
2022-06-28 10:30:25 -0400

    Fixed url and designed

--- population/views.py
+++ population/views.py
@@ -5,17 +5,6 @@ import datetime
 # Create your views here.
 def cities(request):
     cities = Population.objects.all()
-    print("''''''''''''''''''''''''''''''''''''''")
-    print(type(cities))
-    print("*************************************")
-    data = []
-    for city in cities:
-        data += [city.city, city.admin_name, int(city.population)]
-    print("''''''''''''''''''''''''''''''''''''''")
-    print(type(data))
-    now = datetime.datetime.now() 
-
-    msg = f'Today is {now}'
     return render(request, 'landing.html', {"cities": cities})
     # return HttpResponse(msg, content_type='text/plain')
 
@@ -23,4 +12,4 @@ def delete_province(request, data_id):
     province = Population.objects.get(id=data_id)
     province.delete()
     print(data_id)
-    return redirect('/population/')
+    return redirect('/')
